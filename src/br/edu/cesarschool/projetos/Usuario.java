4e81132b0ca33675e5e60facf1b4d8002982e37a
Raphael Barros <100943760+raphaelbalbanez@users.noreply.github.com>
2023-05-23 01:24:11 -0300

    add user

--- src/br/edu/cesarschool/projetos/Usuario.java
+++ src/br/edu/cesarschool/projetos/Usuario.java
@@ -1,5 +1,32 @@
 package br.edu.cesarschool.projetos;
 
 public class Usuario {
-
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String senha;
 }
